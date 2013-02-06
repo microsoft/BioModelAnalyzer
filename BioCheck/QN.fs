e7368b0b6dd0501024174cf4bab275e989664cc4
Samin Ishtiaq <Samin.Ishtiaq@microsoft.com>
2013-02-06 13:30:13 +0000

    Copy over Samin's BioCheck from SD.
    git-tfs-id: [https://vstf-eu-dub-01.partners.extranet.microsoft.com:8443/tfs/EXT04_Consol_TPC1]$/BioCheck;C83747

---
+++ BioCheck/QN.fs
@@ -0,0 +1,94 @@
+﻿(* Copyright (c) Microsoft Corporation. All rights reserved. *)
+module QN
+
+/// A Qualtitative Network.
+
+// Variables are just referenced by their index.
+type var = int
+
+// Return a fresh variable name.
+let mutable num_vars = 0
+let mk_var _ =
+    let n = num_vars
+    num_vars <- num_vars + 1
+    n
+
+let mk_var_unsafe i = 
+    i
+
+/// A node in a QN network.
+type node =
+    {
+        var : var;          // The variable associated with this node's output.
+        f : Expr.expr;      // The target function for this node.
+        inputs : var list;  // The variables this node depends on.
+        range : int * int;  // [min..max]
+        name : string;
+    }
+
+let str_of_node (n:node) = 
+    let ii = String.concat "," (List.map (fun v -> (string)v) n.inputs)                    
+    let (lo,hi) = n.range 
+    let f = Expr.str_of_expr n.f
+    sprintf "{var=%d; range=[%d,%d]; name=%s; inputs={%s}; f=(%s)}" n.var lo hi n.name ii f
+
+type qn = node list 
+    
+/// 
+type interval = Map<var, int*int>
+type range = Map<var,int*int>
+
+type env = Map<var,int>
+
+let str_of_range network range = 
+    let names = Map.ofList (List.map (fun n -> n.var,n.name) network) 
+    String.concat ", " (Map.fold (fun st v (lo,hi) -> (sprintf "%s.%d:[%d,%d]" (Map.find v names) v lo hi)::st) [] range)    
+
+let str_of_env env = 
+    let l = Map.toList env 
+    String.concat ", " (List.map (fun (v,i) -> sprintf "(%d,%d)" v i) l)
+
+/// Well-formed QN network 
+/// Raises exn if qn isn't wf. 
+let qn_wf qn = 
+    // all ids are unique
+    let uniq_ids = List.fold
+                        (fun uniq_ids (n:node) -> 
+                            let n_id = n.var 
+                            match Map.tryFind n_id uniq_ids with 
+                            | None -> Map.add n_id n uniq_ids 
+                            | Some _ -> failwith ("Two entries for " + (string)n_id))
+                        Map.empty 
+                        qn 
+    // Target functions only mention valid ids 
+    List.iter 
+        (fun (n:node) -> 
+            let vv_in_f = Expr.fv n.f
+            let vv_valid = Set.forall (fun v -> Map.containsKey v uniq_ids) vv_in_f 
+            if (not vv_valid ) then 
+                let bad_vv = Set.fold (fun st v -> (string)v + st) "" vv_in_f
+                failwith ("A T input in not a variable: " + bad_vv))
+        qn 
+      
+/// Check that env is complete wrt to qn
+let env_complete_wrt_qn (qn:qn) (env:env) =     
+    if (List.length qn = env.Count) then
+        // Each var \in qn is also defined in env
+        let env_wrt_qn = List.forall (fun (n:node) -> Map.containsKey n.var env) qn
+        // Each var \in env is also defined in qn 
+        let qn_wrt_env = Map.forall (fun v i -> List.exists (fun n -> n.var = v) qn) env
+        // Each var \in env is is correctly bounded
+        let env_bounded = 
+            Map.forall 
+                (fun v i -> 
+                            let n = List.find (fun n -> n.var=v) qn
+                            let min,max = n.range
+                            min <= i && i <= max) 
+                env
+        env_wrt_qn && qn_wrt_env && env_bounded 
+    else false 
+
+
+
+
+
---
+++ BioCheck/UnitTests.fs
@@ -0,0 +1,41 @@
+﻿(* Copyright (c) Microsoft Corporation. All rights reserved. *)
+module UnitTests
+
+open System.Xml
+open System.Xml.Linq
+
+type IA = BioCheckAnalyzerCommon.IAnalyzer2
+
+let register_tests2 (analyzer:UIMain.Analyzer2) =
+    
+    let load_run_check (fname:string) ocheck_stabilizes ocheck_cex = 
+        XDocument.Load(fname)
+        |> Marshal.model_of_xml 
+        |> Stabilize.stabilization_prover
+        |> (fun (sr,cex_o) -> 
+                    match (sr,cex_o) with 
+                     | (Result.SRStabilizing(_), None) -> 
+                            (Option.get ocheck_stabilizes) sr
+                     | (Result.SRNotStabilizing(_), Some(cex)) -> 
+                            (Option.get ocheck_cex) cex 
+                     | _ -> failwith "UnitTests got bad results from checkStabilityOrElseFindCEx")
+
+    Test.register_test true (fun () -> load_run_check "fs_2var_unstable.xml" None (Some Result.bifurcates))
+    Test.register_test true (fun () -> load_run_check "fs_BuddingYeast.xml"  (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_BuddingYeastx2.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_CaitlinToyModel.xml" (Some Result.stabilizes) None) 
+ // Drosophila TO.                     load_run_check
+ //   Test.register_test true (fun () -load_run_checkun "fs_DrosophilaPairRule.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_OnlyInhibitionNonDefault.xml" None (Some Result.cycles)) 
+    Test.register_test true (fun () -> load_run_check "fs_OnlyInhibitionNonStabilizing.xml" None (Some Result.cycles)) 
+    Test.register_test true (fun () -> load_run_check "fs_OnlyInhibitionTest.xml" (Some Result.stabilizes) None)
+    Test.register_test true (fun () -> load_run_check "fs_SkinModel.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_Skinv6.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_ToyModelStable.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_ToyModelUnstable.xml" None (Some Result.cycles))     
+    Test.register_test true (fun () -> load_run_check "fs_VPC_lin15ko.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_VPCwildtype_v1.xml" (Some Result.stabilizes) None) 
+    Test.register_test true (fun () -> load_run_check "fs_biocheck_ex.xml" None (Some Result.cycles)) // SI: changed from fixpoint
+    Test.register_test true (fun () -> load_run_check "fs_biocheck_ex1.xml" (Some Result.stabilizes) None)
+    Test.register_test true (fun () -> load_run_check "fs_diabetes_new_mod.xml" (Some Result.stabilizes) None) 
+
