049be7bc2d55716b64b73793c12338011f6c86f6
sbourton <sbourton@partners.tfs.local>
2013-05-14 09:21:23 +0000

    Updated FSharp PowerPack and Azure 2.0 SDK references. INSTALL THE LATEST AZURE SDK
    git-tfs-id: [https://vstf-eu-dub-01.partners.extranet.microsoft.com:8443/tfs/EXT04_Consol_TPC1]$/BioCheck;C88420

--- BioCheck.Web/Log/ErrorDataSource.cs
+++ BioCheck.Web/Log/ErrorDataSource.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table.DataServices;
