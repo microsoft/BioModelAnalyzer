5578f84d20c88a54b45dbbc53afdc96eceb47b7d
Gavin Smyth <Gavin.Smyth@microsoft.com>
2013-10-15 10:25:54 +0000

    Remove no longer necessary web project files and associated references
    git-tfs-id: [https://vstf-eu-dub-01.partners.extranet.microsoft.com:8443/tfs/EXT04_Consol_TPC1]$/BioCheck;C94319

--- BioCheck.Web/InstallVC.cmd
+++
@@ -1,7 +0,0 @@
-REM see ServiceDefinition.csdef for the definition of the environment variable
-if "%ComputeEmulatorRunning%" == "false" (
-	REM Only run this in the cloud
-	"%~dp0vcredist_x64.exe" /q /norestart
-) ELSE (
-	REM Running locally
-)
