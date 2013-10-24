64863a515b1aad8e0d1897391239a8d746c2fb59
Ben Hall <benhall@microsoft.com>
2013-10-24 17:20:11 +0000

    Basic PDB reader
    git-tfs-id: [https://vstf-eu-dub-01.partners.extranet.microsoft.com:8443/tfs/EXT04_Consol_TPC1]$/BioCheck;C95278

--- Athene/Athene/IO.fs
+++ Athene/Athene/IO.fs
@@ -1,9 +1,19 @@
 ﻿module IO
 
 open System.IO
+open Physics
+open Vector
+
+(*
+'Spherical E. coli' particle
+Particle({x=0.<um>;y=0.<um>;z=0.<um>},{x=0.<um/second>;y=0.<um/second>;z=0.<um/second>}, 0.00002<second>, 0.7<um>, 1.3<pg um^-3>, false)
+*)
 
 let dropFrame (system: Physics.Particle list) =
     ()
+
+let cart2Particle ((name:string), (xr:float), (yr:float), (zr:float)) = 
+    Particle(name,{x=(xr*1.<um>);y=(yr*1.<um>);z=(zr*1.<um>)},{x=0.<um/second>;y=0.<um/second>;z=0.<um/second>}, 1.<second>, 1.<um>, 1.<pg um^-3>, false)
 
 let xyzWriteFrame (filename: string) (system: Physics.Particle list) =
         use file = new StreamWriter(filename, true)
@@ -14,7 +24,14 @@ let xyzWriteFrame (filename: string) (system: Physics.Particle list) =
         file.Close()
 
 let pdbRead (filename: string) =
-        use file = new StreamReader(filename)
-        let system = [for line in file.ReadLine() -> line]
-        file.Close()
-        system
+    let atomParse (line: string) = 
+        let name =    (line.Substring (11,5)).Trim()
+        let x = float (line.Substring (30,8))
+        let y = float (line.Substring (38,8))
+        let z = float (line.Substring (46,8))
+        cart2Particle (name, x,y,z)
+    [for line in File.ReadLines(filename) do match line with
+                                                            | atom when atom.StartsWith("ATOM") -> yield atomParse line
+                                                            | _ -> () ]
+    
+    
