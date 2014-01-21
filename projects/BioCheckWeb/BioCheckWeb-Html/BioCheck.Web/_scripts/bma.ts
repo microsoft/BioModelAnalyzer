583d9ded09d41db8222feb9d3727b391e84a2515
Gavin Smyth <Gavin.Smyth@microsoft.com>
2014-01-21 15:33:47 +0000

    Tidy up SVG class manipulation
    git-tfs-id: [https://vstf-eu-dub-01.partners.extranet.microsoft.com:8443/tfs/EXT04_Consol_TPC1/]$/Biocheck;C107556

--- projects/BioCheckWeb/BioCheckWeb-Html/BioCheck.Web/_scripts/bma.ts
+++ projects/BioCheckWeb/BioCheckWeb-Html/BioCheck.Web/_scripts/bma.ts
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+  Copyright 2014 Microsoft Corporation.  All Rights Reserved.
+
+  Core BMA web UI
+
+******************************************************************************/
+
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="jqueryui/jqueryui.d.ts" />
 // /// <reference path="vuePlotTypes.ts" />
@@ -9,7 +17,6 @@ window.onload = () => {
     var svgjq = $("#svgroot");
     // Indirection via <any> to stop compiler complaining :-|
     svg = <any>svgjq[0];
-    //svg = <any>document.getElementById("svgroot");
     svgjq.mousedown(startDrag);
     svgjq.mousemove(doDrag);
     svgjq.mouseup(drawItemOrStopDrag);
@@ -381,17 +388,21 @@ function createHighlightableSvgGroup(children: SVGElement[], x: number, y: numbe
     highlightPath.setAttribute("stroke-width", (3 / scale) + "px");
     highlightPath.setAttribute("stroke", "transparent");
     var group = createSvgGroup(children, x, y, scale);
-    group.setAttribute("onmouseover", "this.childNodes[0].setAttribute('stroke', 'gray')");
-    group.setAttribute("onmouseout", "this.childNodes[0].setAttribute('stroke', 'transparent')");
+    //group.setAttribute("onmouseover", "this.childNodes[0].setAttribute('stroke', 'gray')");
+    //group.setAttribute("onmouseout", "this.childNodes[0].setAttribute('stroke', 'transparent')");
+    group.setAttribute("onmouseover", "svgAddClass(this.childNodes[0], 'svg-highlight')");
+    group.setAttribute("onmouseout", "svgRemoveClass(this.childNodes[0], 'svg-highlight')");
     // Allow the invisible stroke to still participate in hit testing
     group.setAttribute("pointer-events", "all");
-    group.setAttribute("class", "shape");
+    //group.setAttribute("class", "shape");
+    svgAddClass(group, "shape");
     return group;
 }
 
 function createTopGroupAndAdd(children: SVGElement[], x: number, y: number) {
     var elem = createSvgGroup(children, x, y);
-    elem.setAttribute("class", "object");
+    //elem.setAttribute("class", "object");
+    svgAddClass(elem, "object");
     svg.appendChild(elem);
     return elem;
 }
@@ -452,6 +463,32 @@ function getTrueBBox(elem: SVGGElement) {
     return { x: x, y: y, width: box.width, height: box.height };
 }
 
+// SVG class seems to be treated as a "normal" string attribute in all but the
+// most recent IE, so roll our own class manipulation
+
+function stringInString(s: string, find: string) {
+    return s.match(new RegExp("(\\s|^)" + find + "(\\s|$)"));
+}
+
+function svgHasClass(elem: SVGStylable, c: string) {
+    return stringInString(elem.className.baseVal, c);
+}
+
+function svgAddClass(elem: SVGStylable, c: string) {
+    var s = elem.className.baseVal;
+    if (!s)
+        elem.className.baseVal = c;
+    else if (!stringInString(s, c))
+        elem.className.baseVal = s + " " + c;
+}
+
+function svgRemoveClass(elem: SVGStylable, c: string) {
+    var s = elem.className.baseVal.replace(new RegExp("(\\s|^)" + c + "(\\s|$)"), " ");
+    if (s == " ")
+        s = null;
+    elem.className.baseVal = s;
+}
+
 function addItem(type: ItemType, pt: Point, elemAndPart: ElementAndPart): Item {
     switch (type) {
         case ItemType.Container:
@@ -478,11 +515,14 @@ function addContainer(x: number, y: number) {
     var container = new Container();
 
     var outerPath = createSvgPath("M3.6-49.9c-26.7,0-48.3,22.4-48.3,50c0,27.6,21.6,50,48.3,50c22.8,0,41.3-22.4,41.3-50C44.9-27.5,26.4-49.9,3.6-49.9z", "#FAAF42");
-    outerPath.setAttribute("class", "cell-outer");
+    //outerPath.setAttribute("class", "cell-outer");
+    svgAddClass(outerPath, "cell-outer");
     var innerPath = createSvgPath("M3.6,45.5C-16.6,45.5-33,25.1-33,0.1c0-25,16.4-45.3,36.6-45.3c20.2,0,36.6,20.3,36.6,45.3C40.2,25.1,23.8,45.5,3.6,45.5z", "#FFF");
-    innerPath.setAttribute("class", "cell-inner");
+    //innerPath.setAttribute("class", "cell-inner");
+    svgAddClass(innerPath, "cell-inner");
     var graphic = createHighlightableSvgGroup([outerPath, innerPath], 0, 0, 2.5);
-    graphic.setAttribute("class", "shape");
+    //graphic.setAttribute("class", "shape");
+    svgAddClass(graphic, "shape");
     var text = createSvgText(container.name, -100, -125); // offset...
     var elem = createTopGroupAndAdd([graphic, text], x, y);
     container.element = elem;
@@ -496,7 +536,8 @@ function addVariable(x: number, y: number) {
     var variable = new Variable(ItemType.Variable);
     var path = createSvgPath("M27.3,43.4l-2.2-0.8c-12-4.4-19.3-11.5-20-19.7c0-0.5-0.1-0.9-0.1-1.4c-5.4-2.6-9-7.3-10.5-12.3c-0.6-2-0.9-4.1-0.8-6.3c-4.7-1.7-8.2-4.7-10.3-8.2c-2.1-3.4-3.2-8.1-2.1-13.4c-6.7-1.8-12.5-4.3-15.9-5.8l-7.4,19.9l26.7,7.9L-17,9.1l-32.8-9.7l11.9-32l3,1.5c3.9,1.9,10.8,4.9,18.1,6.9c1.9-4,5.1-8.1,10-12.1c10.8-8.9,19.7-8.1,23.8-3.4c3.5,4,3.6,11.6-4.2,18.7c-6.3,5.7-16.2,5.7-25.7,3.8c-0.6,3.2-0.2,6.2,1.4,8.9c1.3,2.2,3.4,4,6.3,5.3C-3.4-8.3,0.7-13.2,8-16c15.9-6.1,19.9,0.2,20.7,2.2c2.1,5.2-2.4,11.8-10.1,15C11.5,4.2,5.1,5-0.3,4.4C-0.2,5.5,0,6.5,0.3,7.5c0.9,3.2,3,6.1,6.2,8C8,12.1,11,9,15,6.7C25,1,32.2,1.6,35.7,4.2c2.3,1.7,3.3,4.3,2.7,7.1c-1.1,5.3-7.6,9.7-17.5,11.8c-3.6,0.8-6.8,0.8-9.7,0.4c1,4.9,6,9.5,13.9,12.8l7.4-10.7l17.4,10.1l-3,5.1l-12.6-7.4L27.3,43.4L27.3,43.4z M12.1,17.5c2.2,0.3,4.8,0.3,7.6-0.3c9.4-2,12.6-5.6,12.9-7.2c0.1-0.4,0-0.7-0.4-1c-1.4-1-6.2-1.7-14.1,2.9C15.2,13.4,13.2,15.4,12.1,17.5L12.1,17.5z M0.6-1.5C5-1,10.3-1.7,16.3-4.2c5.4-2.3,7.4-6,6.9-7.3c-0.4-1-4.3-2.2-13,1.1C5-8.5,2-5.1,0.6-1.5L0.6-1.5z M-10.8-22.8c7.8,1.4,15.2,1.3,19.5-2.6c4.7-4.2,5.4-8.4,3.7-10.4c-2.1-2.5-8.3-1.9-15.5,4.1C-6.5-28.9-9.1-25.9-10.8-22.8L-10.8-22.8z", "#EF4137");
     var graphic = createHighlightableSvgGroup([path], 0, 0, 0.36);
-    graphic.setAttribute("class", "shape");
+    //graphic.setAttribute("class", "shape");
+    svgAddClass(graphic, "shape");
     var text = createSvgText(variable.name, 0, 50); // offset...
     variable.element = createTopGroupAndAdd([graphic, text], x, y);
 
@@ -509,7 +550,8 @@ function addConstant(x: number, y: number) {
     var constant = new Variable(ItemType.Constant);
     var path = createSvgPath("M27.3,43.4l-2.2-0.8c-12-4.4-19.3-11.5-20-19.7c0-0.5-0.1-0.9-0.1-1.4c-5.4-2.6-9-7.3-10.5-12.3c-0.6-2-0.9-4.1-0.8-6.3c-4.7-1.7-8.2-4.7-10.3-8.2c-2.1-3.4-3.2-8.1-2.1-13.4c-6.7-1.8-12.5-4.3-15.9-5.8l-7.4,19.9l26.7,7.9L-17,9.1l-32.8-9.7l11.9-32l3,1.5c3.9,1.9,10.8,4.9,18.1,6.9c1.9-4,5.1-8.1,10-12.1c10.8-8.9,19.7-8.1,23.8-3.4c3.5,4,3.6,11.6-4.2,18.7c-6.3,5.7-16.2,5.7-25.7,3.8c-0.6,3.2-0.2,6.2,1.4,8.9c1.3,2.2,3.4,4,6.3,5.3C-3.4-8.3,0.7-13.2,8-16c15.9-6.1,19.9,0.2,20.7,2.2c2.1,5.2-2.4,11.8-10.1,15C11.5,4.2,5.1,5-0.3,4.4C-0.2,5.5,0,6.5,0.3,7.5c0.9,3.2,3,6.1,6.2,8C8,12.1,11,9,15,6.7C25,1,32.2,1.6,35.7,4.2c2.3,1.7,3.3,4.3,2.7,7.1c-1.1,5.3-7.6,9.7-17.5,11.8c-3.6,0.8-6.8,0.8-9.7,0.4c1,4.9,6,9.5,13.9,12.8l7.4-10.7l17.4,10.1l-3,5.1l-12.6-7.4L27.3,43.4L27.3,43.4z M12.1,17.5c2.2,0.3,4.8,0.3,7.6-0.3c9.4-2,12.6-5.6,12.9-7.2c0.1-0.4,0-0.7-0.4-1c-1.4-1-6.2-1.7-14.1,2.9C15.2,13.4,13.2,15.4,12.1,17.5L12.1,17.5z M0.6-1.5C5-1,10.3-1.7,16.3-4.2c5.4-2.3,7.4-6,6.9-7.3c-0.4-1-4.3-2.2-13,1.1C5-8.5,2-5.1,0.6-1.5L0.6-1.5z M-10.8-22.8c7.8,1.4,15.2,1.3,19.5-2.6c4.7-4.2,5.4-8.4,3.7-10.4c-2.1-2.5-8.3-1.9-15.5,4.1C-6.5-28.9-9.1-25.9-10.8-22.8L-10.8-22.8z", "#BBBDBF");
     var graphic = createHighlightableSvgGroup([path], 0, 0, 0.36);
-    graphic.setAttribute("class", "shape");
+    //graphic.setAttribute("class", "shape");
+    svgAddClass(graphic, "shape");
     var text = createSvgText(constant.name, 0, 50); // offset...
     constant.element = createTopGroupAndAdd([graphic, text], x, y);
     //constant.element.setAttribute("data-object", constant);
@@ -523,7 +565,8 @@ function addReceptor(x: number, y: number) {
     var receptor = new Variable(ItemType.Constant);
     var path = createSvgPath("M9.9-10.5c-1.4-1.9-2.3,0.1-5.1,0.8C2.6-9.2,2.4-13.2,0-13.2c-2.4,0-2.4,3.5-4.8,3.5c-2.4,0-3.8-2.7-5.2-0.8l8.2,11.8v12.1c0,1,0.8,1.7,1.7,1.7c1,0,1.7-0.8,1.7-1.7V1.3L9.9-10.5z", "#3BB34A");
     var graphic = createHighlightableSvgGroup([path], 0, 0, 1);
-    graphic.setAttribute("class", "shape");
+    //graphic.setAttribute("class", "shape");
+    svgAddClass(graphic, "shape");
     receptor.element = createTopGroupAndAdd([graphic], x, y);
 
     //container.children.push(receptor);
