c6a99482b2ca4af8464771eaa7be95ad0f36680a
Nir Piterman <nir.piterman@le.ac.uk>
2014-04-25 16:46:08 +0000

    created birth files
    git-svn-id: https://intranet.cs.le.ac.uk/svn/np183/trunk@3958 0e12c734-ef1f-0410-b975-b390c3a2b4f8

---
+++ Event/Birth.cpp
@@ -0,0 +1,18 @@
+/*
+ * Birth.cpp
+ *
+ *  Created on: 25 Apr 2014
+ *      Author: np183
+ */
+
+#include "Birth.h"
+
+Birth::Birth() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Birth::~Birth() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Event/Birth.h
@@ -0,0 +1,38 @@
+/*
+ * Birth.h
+ *
+ *  Created on: 25 Apr 2014
+ *      Author: np183
+ */
+
+#ifndef BIRTH_H_
+#define BIRTH_H_
+
+#include <string>
+#include <iosfwd>
+#include "Event.h"
+
+class Birth: public Event {
+public:
+	Birth() =delete;
+	Birth(const std::string me, State* st, float d, float t, Simulation* s, Cell* c);
+
+	virtual ~Birth();
+
+	std::string cell() const;
+
+	void setCell(const std::string& c);
+
+	virtual std::vector<Event*> excute() const;
+	virtual void output(std::ostream&) const;
+	virtual void concerns(const std::string&) const;
+
+	virtual std::string toString() const;
+
+	friend std::ostream& operator<<(std::ostream&, const Birth&);
+private:
+	std::string _cell;
+	State* _st;
+};
+
+#endif /* BIRTH_H_ */
