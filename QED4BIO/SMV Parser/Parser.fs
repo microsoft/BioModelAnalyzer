d26af7421711bb84f5873383d8922781fd451034
mattpark <mattpark@microsoft.com>
2014-05-06 17:29:42 +0100

    Initial hash of the parser

---
+++ QED4BIO/SMV Parser/Parser.fs
@@ -0,0 +1,130 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+module Parser 
+
+open FParsec.CharParsers
+open FParsec
+
+let ws : Parser<unit,unit> = many 
+                                (skipString "--" .>> (skipRestOfLine true)
+                                <|> spaces1) >>% ()
+
+//let skipStringNS = skipString
+let skipString a =  skipString a .>> ws
+let pint64 = pint64 .>> ws
+
+let kwModule = skipString "MODULE"
+let kwVar = skipString "VAR"
+let kwAssign = skipString "ASSIGN"
+let kwinit = skipString "init"
+let kwnext = skipString "next"
+let kwInit = skipString "INIT"
+let kwTrans = skipString "TRANS"
+let kwcase = skipString "case"
+let kwesac = skipString "esac"
+
+let keywords = ["MODULE"; "VAR"; "ASSIGN"; "init"; "next"; "INIT"; "TRANS"; "case"; "esac"]
+
+let pident : Parser<string,unit> = attempt (many1Satisfy2 (fun c -> isLetter c) (fun c -> isLetter c || isDigit c || '_' = c ) >>= fun s -> if List.exists ( (=) s) keywords then pzero else preturn s) .>> ws  
+
+let plist b e s pp = between (skipString b) (skipString e) (sepBy pp (skipString s))
+
+let parguments pp = plist "(" ")" "," pp
+
+let parguments_opt pp =  opt (parguments pp)
+
+let pidentexpr = sepBy pident (skipString ".")
+
+type types = Set of string list
+           | Module of string * (string list) list option 
+           | Range of int64 * int64
+let ptype = 
+    (plist "{" "}" "," pident |>> Set)
+    <|> (pident .>>. parguments_opt pidentexpr |>> Module)
+    <|> (pint64 .>> skipString ".." .>>. pint64 |>> Range)
+
+let pvardecl = pident .>> skipString ":" .>>. ptype .>> skipString ";"
+
+let pvardecls = many1 pvardecl
+
+type expr = 
+    | Next of expr
+    | Not of expr 
+    | Ident of string
+    | And of expr * expr
+    | Or of expr * expr
+    | Imp of expr * expr
+    | Lt of expr * expr
+    | Le of expr * expr
+    | Ge of expr * expr
+    | Gt of expr * expr
+    | Eq of expr * expr
+    | Neq of expr * expr 
+    | Add of expr * expr 
+    | Int of int64
+    | Cases of (expr * expr) list
+
+let opp = new OperatorPrecedenceParser<expr,unit,unit>()
+let pexpr = opp.ExpressionParser .>> ws
+
+(* TODO What is the actual operator precedence for SMV *)
+let _ = opp.AddOperator(PrefixOperator("!", ws, 6, false, Not))
+let _ = opp.AddOperator(InfixOperator("|", ws, 2, Associativity.Left, fun x y-> Or(x,y)))
+let _ = opp.AddOperator(InfixOperator("&", ws, 3, Associativity.Left, fun x y-> And(x,y)))
+let _ = opp.AddOperator(InfixOperator("->", ws, 1, Associativity.Left, fun x y-> Imp(x,y)))
+
+
+let _ = opp.AddOperator(InfixOperator("<", ws, 4, Associativity.Left, fun x y-> Lt(x,y)))
+let _ = opp.AddOperator(InfixOperator("<=", ws, 4, Associativity.Left, fun x y-> Le(x,y)))
+let _ = opp.AddOperator(InfixOperator(">", ws, 4, Associativity.Left, fun x y-> Gt(x,y)))
+let _ = opp.AddOperator(InfixOperator(">=", ws, 4, Associativity.Left, fun x y-> Ge(x,y)))
+let _ = opp.AddOperator(InfixOperator("=", ws, 4, Associativity.Left, fun x y-> Eq(x,y)))
+let _ = opp.AddOperator(InfixOperator("!=", ws, 4, Associativity.Left, fun x y-> Neq(x,y)))
+
+let _ = opp.AddOperator(InfixOperator("+", ws, 6, Associativity.Left, fun x y-> Add(x,y)))
+
+let pcases =
+    between kwcase kwesac (many1 (pexpr .>> skipString ":" .>>. pexpr .>> skipString ";"))
+        |>> Cases
+
+let pnext = kwnext >>. between (skipString "(") (skipString ")") pexpr |>> Next
+
+let pparen = between (skipString "(") (skipString ")") pexpr
+
+let pint : Parser<expr, unit> = pint64 |>> Int
+
+let _ = opp.TermParser <- pint <|> pnext <|> pparen <|> pcases <|> (pident |>> Ident)
+
+let pdefn pkw pbody =
+    pkw >>. skipString "(" >>. pident .>> skipString ")" .>> 
+        skipString ":=" .>>. pbody .>> skipString ";"
+
+type assign = 
+    | InitAssign of string * expr
+    | NextAssign of string * expr
+
+let pinitvar = pdefn kwinit pexpr |>> InitAssign
+
+let pupdatevar = pdefn kwnext pexpr |>> NextAssign
+
+let pupdate = 
+      pinitvar
+      <|> pupdatevar  
+
+type section = 
+    | Assigns of assign list
+    | Init of expr
+    | Trans of expr
+    | Var of (string * types) list
+
+let pVarSec = kwVar >>. pvardecls |>> Var
+let pTransSec = kwTrans >>. pexpr |>> Trans
+let pInitSec = kwInit >>. pexpr |>> Init
+let pAssignSec = kwAssign >>. many pupdate |>> Assigns
+
+let pSecs = pVarSec <|> pTransSec <|> pInitSec <|> pAssignSec
+
+let pModule =
+    kwModule >>. pident .>>. (parguments_opt pident) .>>. many pSecs 
+
+let pSmv = (ws >>. many pModule .>> eof)
