e6849d40e6e8c51378f235dcf915caea6b8fb96a
mattpark <mattpark@microsoft.com>
2014-07-16 15:49:53 +0100

    Implemented the simulator by calling Z3 in an all_smt loop.  Currently fragile, as a bad formula will cause non-termination.

--- QED4BIO/Automata/Program.fs
+++ QED4BIO/Automata/Program.fs
@@ -12,53 +12,89 @@ let show_automata (a : Automata<_,_>) =
     let graph = new Microsoft.Msagl.Drawing.Graph()
     a.Graph(graph) |> ignore
     gviewer.Graph <- graph
+    gviewer.Dock <- DockStyle.Fill
     form.Controls.Add(gviewer)
+    
     do Application.Run(form)
 
 
 [<EntryPoint>]
 let main argv = 
-    let a = new SimpleAutomata<string>()
-    a.addState(0,"a")
-    a.addInitialState(0)
-    a.addState(1,"a")
-    a.addEdge(0,1)
-    a.addState(2,"d")
-    a.addEdge(1,2)
-    a.addState(3,"d")
-    a.addEdge(2,3)
-    a.addState(4,"d")
-    a.addEdge(3,4)
-    a.addEdge(4,4)
-
-    show_automata a
-    
-    let b = new BoundedAutomata<int, string>(1, a)
-    show_automata b
-
-    let b = compressedMapAutomata( b, fun x -> x)
-    show_automata b
-
-    let a = new SimpleAutomata<string>()
+//    let a = new SimpleAutomata<string>()
+//    a.addState(0,"a")
+//    a.addInitialState(0)
+//    a.addState(1,"a")
+//    a.addEdge(0,1)
+//    a.addState(2,"d")
+//    a.addEdge(1,2)
+//    a.addState(3,"d")
+//    a.addEdge(2,3)
+//    a.addState(4,"d")
+//    a.addEdge(3,4)
+//    a.addEdge(4,4)
+//
+//    show_automata a
+//    
+//    let b = new BoundedAutomata<int, string>(1, a)
+//    show_automata b
+//
+//    let b = compressedMapAutomata( b, fun x -> x)
+//    show_automata b
+//
+//    let a = new SimpleAutomata<string>()
+//    for i = 1 to 4 do
+//        a.addInitialState i
+//        a.addState(i,i.ToString())
+//        if i<4 then a.addEdge(i,i+1)
+//    show_automata a
+//
+//    let b = new BoundedAutomata<int, string>(6, a)
+//    show_automata b
+//
+//    let b = compressedMapAutomata(new BoundedAutomata<int, string>(6, a), fun x -> x)
+//    show_automata b
+//
+//    for i = 1 to 20 do
+//       let start = System.DateTime.Now
+//       compressedMapAutomata(new BoundedAutomata<int, string>(i, a), fun x -> x) |> ignore
+//       let finish = System.DateTime.Now
+//       let duration = finish.Ticks - start.Ticks
+//       printfn "Round %d Ticks %d" i duration
+//
+//    show_automata (Simulator.test_automata ())
+//
+//    show_automata (Simulator.test_automata2 ())
+//
+    let a = new SimpleAutomata<Simulator.interp>()
+    let b = Map.add "input" 1 Map.empty
     for i = 1 to 4 do
-        a.addInitialState i
-        a.addState(i,i.ToString())
-        if i<4 then a.addEdge(i,i+1)
+        for j = 1 to 4 do
+            a.addInitialState i
+            a.addState(i,Map.add "neighbour_path" i b)
+            a.addEdge(i,j)
+    
     show_automata a
 
-    let b = new BoundedAutomata<int, string>(6, a)
-    show_automata b
+    let t1 = Simulator.test_automata3 a
 
-    let b = compressedMapAutomata(new BoundedAutomata<int, string>(6, a), fun x -> x)
-    show_automata b
+    show_automata t1
+    
+    let t2 = compressedMapAutomata(t1, fun m -> Map.add "neighbour_path" (fst m).["path"] b)
 
-    for i = 1 to 20 do
-       let start = System.DateTime.Now
-       compressedMapAutomata(new BoundedAutomata<int, string>(i, a), fun x -> x) |> ignore
-       let finish = System.DateTime.Now
-       let duration = finish.Ticks - start.Ticks
-       printfn "Round %d Ticks %d" i duration
+    show_automata t2
 
+    let t2 = new BoundedAutomata<int,Simulator.interp> (1,t2)
+
+    show_automata t2
+
+    let t2 = compressedMapAutomata(t2, fun m -> m)
+
+    show_automata t2
+    
+
+    let t3 = Simulator.test_automata3 t2
+
+    show_automata t3
 
 
     printfn "%A" argv
--- QED4BIO/Automata/Simulator.fs
+++ QED4BIO/Automata/Simulator.fs
@@ -1,51 +1,199 @@
 ﻿module Simulator
 open Automata
+open Microsoft.Z3
 
-type formula = unit
+type formula = Microsoft.Z3.BoolExpr
 type interp = Map<string, int>
 
-let all_smt (f : formula) : Set<interp> = Set.empty
+let context = new Microsoft.Z3.Context()
+let (==>) x y = context.MkImplies (x,y)
+let (&&&) x y = context.MkAnd(x,y)
+let (|||) x y = context.MkOr(x,y)
+let (===) x y = context.MkEq(x,y)
+let (<<)  x y = context.MkLt(x,y)
+let (++)  x y = context.MkAdd(x,y)
 
-let add_interp (f : formula) (baseinterp : interp) : formula  = ()
-
-
-let sim initform stepformula (rely : Automata<'istate,interp>) = 
-    let normalize =
-        let table = new System.Collections.Generic.Dictionary<interp * 'istate,  int>()
-        let index = ref 0
-        fun x -> 
-            match table.TryGetValue x with
-            | true, y -> y
-            | false, _ -> 
-                let r = !index
-                table.Add(x,r)
-                index := r + 1
-                r
-    let result = new SimpleAutomata<(interp * 'istate)>()
-
-    for interp in all_smt initform do
-        for index in rely.initialstates do
-            result.addInitialState (normalize (interp,index))
-            result.addState((normalize (interp,index)), (interp,index))
+let not x = context.MkNot(x)
+let int (i : int) = context.MkInt(i)
+let var (s : string) = context.MkIntConst(s)
     
-    let work_set = System.Collections.Concurrent.ConcurrentBag()
+let next_pre = "next_"
+let prev_pre = "prev_"
 
-    for v in result.initialstates do 
-        work_set.Add (result.value v)
+///Returns the interpretation of the new state in this model
+let get_interp (m : Model) : interp = 
+    let mutable interp = Map.empty
+    for fd in m.ConstDecls do
+        let constant = fd.Name.ToString()
+        if constant.StartsWith(next_pre) then
+             match m.ConstInterp fd with
+             | :? IntNum as i  -> 
+                let constant = constant.Substring(next_pre.Length)
+                interp <- Map.add constant i.Int interp
+             | _ -> ()
+    interp
+
+///Creates the formula representing this variable assignment
+let interp_form (i : interp) s : formula = 
+    let assigns = 
+        [| 
+            for j,k in Map.toSeq i do
+                yield var (s + j) === int k
+        |]
+    context.MkAnd(assigns)
+
+
+///Stateful function to provide an all_smt loop given an interpretation of some of the variables
+///Recommended use
+///    let foo = all_smt foo_form
+///    let r1  = foo i1
+///    let r2  = foo i2
+///    ...
+let all_smt (f : formula) : interp list -> Set<interp> =
+    //Construct Z3 context with the formula
+    let solver = context.MkSimpleSolver()
+    solver.Assert(f)
+    fun base_interps -> 
+        //Push interpretation of some variables to formula
+        solver.Push()
+        for i in base_interps do
+            solver.Assert(interp_form i prev_pre)
+        //Loop finding new interpretations of remaining variables 
+        let mutable new_interps = Set.empty
+        while solver.Check([||]) = Microsoft.Z3.Status.SATISFIABLE do
+            //Evaluate the interpretation
+            let next_interp = get_interp solver.Model
+            new_interps <- Set.add next_interp new_interps
+            //Assert negation of interpretation
+            solver.Assert(context.MkNot (interp_form next_interp next_pre))
+
+        //Clear the interpretation
+        solver.Pop()
+        //Return all the interpretations
+        new_interps
+
+///Stateful function to simulate stuff.  The first two arguments establish the Z3 context, so recommended use is
+///  let sim1 = sim initform stepformula
+///  let t1 = sim1 R0
+///  let t2 = sim1 R1
+///  ...
+///  let t3 = sim1 Rn 
+let sim initform stepformula =
+    //Prebuild set of initial states from formula
+    let init_interps = all_smt initform []
+
+    //Build a context for getting the steps, but delay the interpretation allows reuse
+    let steps = all_smt stepformula
+
+    fun (rely : Automata<'istate,interp>) ->
+        ///Stateful function to normalize the states to integers
+        let normalize =
+            let table = new System.Collections.Generic.Dictionary<interp * 'istate,  int>()
+            let index = ref 0
+            fun x -> 
+                match table.TryGetValue x with
+                | true, y -> y
+                | false, _ -> 
+                    let r = !index
+                    table.Add(x,r)
+                    index := r + 1
+                    r
+        let result = new SimpleAutomata<(interp * 'istate)>()
+
+        for interp in init_interps do
+            for index in rely.initialstates do
+                result.addInitialState (normalize (interp,index))
+                result.addState((normalize (interp,index)), (interp,index))
     
-    let mutable more, interp_index = work_set.TryTake()
-    while more do
-        let index = snd interp_index
-        let interp = fst interp_index
-        let new_indexs = rely.next(index)
-        for new_interp in all_smt (add_interp (add_interp (stepformula) interp) (rely.value index))do
-            for new_index in new_indexs do
-                result.addEdge ((normalize (interp,index)), (normalize (new_interp,new_index)))
-                if Set.contains (normalize (new_interp,new_index)) result.states then
-                    ()
-                else
-                    result.addState ((normalize (new_interp,new_index)), (new_interp,new_index))
-                    work_set.Add( (new_interp,new_index) )
-        more <- work_set.TryTake(&interp_index)
+        let work_set = System.Collections.Concurrent.ConcurrentBag()
 
-    result
+        for v in result.initialstates do 
+            work_set.Add (result.value v)
+    
+        let mutable more, interp_index = work_set.TryTake()
+        while more do
+            let index = snd interp_index
+            let interp = fst interp_index
+            let new_indexs = rely.next(index)
+            for new_interp in steps [interp ; rely.value index] do
+                for new_index in new_indexs do
+                    result.addEdge ((normalize (interp,index)), (normalize (new_interp,new_index)))
+                    if Set.contains (normalize (new_interp,new_index)) result.states then
+                        ()
+                    else
+                        result.addState ((normalize (new_interp,new_index)), (new_interp,new_index))
+                        work_set.Add( (new_interp,new_index) )
+            more <- work_set.TryTake(&interp_index)
+
+        result
+
+
+let test_automata () = 
+    let rely = new Automata.SimpleAutomata<interp>()
+    rely.addInitialState(0);
+    rely.addState(0, Map.empty)
+    rely.addEdge(0,0)
+    sim 
+        (context.MkEq(context.MkIntConst("next_x"), context.MkInt(0)))
+        (context.MkOr(
+            context.MkEq(context.MkIntConst("next_x"), context.MkInt(2)),
+            context.MkEq(context.MkIntConst("next_x"), context.MkInt(1))
+        ))  
+        rely  
+
+let test_automata2 () = 
+    let rely = new Automata.SimpleAutomata<interp>()
+    rely.addInitialState(0);
+    rely.addState(0, Map.empty)
+    rely.addEdge(0,0)
+    sim 
+        (context.MkEq(context.MkIntConst("next_x"), context.MkInt(0)))
+        (context.MkAnd(
+            context.MkImplies(
+                context.MkLt(context.MkIntConst("prev_x"), context.MkInt(4)),
+                context.MkEq(context.MkIntConst("next_x"), context.MkAdd(context.MkIntConst("prev_x"), context.MkInt(1)))
+            ),
+            context.MkImplies(
+                context.MkEq(context.MkIntConst("prev_x"), context.MkInt(4)),
+                context.MkEq(context.MkIntConst("next_x"), context.MkInt(1))
+            )
+        ))  
+        rely  
+
+
+let test_automata3 a = 
+    
+    sim 
+        (interp_form (Map.ofList [("path",1); ("signal",3)]) next_pre)
+        (context.MkAnd 
+            [|
+            ((var "prev_path" === int 0) ||| (var "prev_path" === int 4)) 
+                ==> (var "prev_path" === var "next_path")
+
+            ((var "prev_path" << int 4) &&& (int 0 << var "prev_path") &&& (var "next_signal" === int 4))
+                ==> (int 0 === (var "next_path"))
+
+            ((var "prev_path" << int 4) &&& (int 0 << var "prev_path") &&& (var "next_signal" << int 4) &&& (var "prev_input" === int 1))
+                ==> (int 4 === (var "next_path"))
+
+            ((var "prev_path" << int 4) &&& (int 0 << var "prev_path") &&& (var "next_signal" << int 4) &&& (var "prev_input" === int 0))
+                ==> (((var "prev_path") ++ (int 1)) === (var "next_path"))
+
+            ((var "prev_neighbour_path" === int 4)  &&& (int 0 << var "prev_signal"))
+                ==> (var "next_signal" === int 4)
+
+            ((var "prev_neighbour_path" << int 4)  &&& (int 4 === var "prev_path"))
+                ==> (var "next_signal" === int 0)
+
+            ((var "prev_neighbour_path" << int 4)  &&& (var "prev_path" << int 4) &&& (int 0 << var "prev_signal") &&& (var "prev_signal" << int 4))
+                ==> ((var "next_signal" ++ int 1) === var "prev_signal")
+
+            ((var "prev_neighbour_path" << int 4)  &&& (var "prev_path" << int 4) &&& ((int 0 === var "prev_signal") ||| (var "prev_signal" === int 4)))
+                ==> (var "next_signal" === var "prev_signal")
+
+            ((var "prev_signal" === int 0)  ==> (var "next_signal" === int 0))
+            |]
+        )  
+        a
+
+
