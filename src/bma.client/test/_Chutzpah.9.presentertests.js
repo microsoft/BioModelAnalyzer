7df9bad7389c3fcef2f9346a24eae38f62b532be
unknown <innaz@beast.mslab.cs.msu.su>
2014-08-25 11:16:09 +0400

    fixed chrome float bug

---
+++ src/bma.client/test/_Chutzpah.9.presentertests.js
@@ -0,0 +1,34 @@
+describe("SVGPlot", function () {
+    it("should be succesfully created", function () {
+        var svgPlot = new BMAExt.SVGPlot($("<div></div>"), undefined);
+        expect(svgPlot).toBeDefined();
+    });
+});
+
+describe("DesignSurfacePresenter", function () {
+    it("should be created from BioModel, Layout and ISVGPlot driver instance", function () {
+        var appModel = new BMA.Model.AppModel();
+        var svgPlotDriver = new BMA.Test.TestSVGPlotDriver();
+
+        //var presenter = new BMA.Presenters.DesignSurfacePresenter(appModel, svgPlotDriver, undefined, undefined);
+        //expect(presenter).toBeDefined();
+        window.Commands = new BMA.CommandRegistry();
+        window.ElementRegistry = new BMA.Elements.ElementsRegistry();
+        var testbutton = new BMA.Test.TestUndoRedoButton();
+        var presenter = new BMA.Presenters.DesignSurfacePresenter(appModel, svgPlotDriver, {
+            GetDragSubject: function () {
+                this.subscribe = function () {
+                };
+            }
+        }, testbutton, testbutton);
+        expect(presenter).toBeDefined();
+    });
+
+    it("should create proper SVG for specified model and layout", function () {
+    });
+
+    it("creates drawingsurface widget", function () {
+        var ds = $("<div id='DRAWINGSURFACE'></div>");
+        ds.drawingsurface();
+    });
+});
