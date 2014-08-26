09f3e878f0bbd7c862db2906dc6883bcaba0056b
Sergey Berezin <b-sebere@microsoft.com>
2014-08-26 12:30:09 +0400

    Service calls actual analyzer.

--- src/bma.client/AnalyzeController.cs
+++ src/bma.client/AnalyzeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BioCheckAnalyzerCommon;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,16 +16,19 @@ namespace bma.client
     public class AnalyzeController : ApiController
     {
         // POST api/Analyze
-        public AnalysisOutput Post([FromBody]AnalysisInput test)
-        {/*
-            var inputXml = XDocument.Parse(new XmlSerializer(typeof(AnalysisInput)).ToStrin);
+        public AnalysisOutput Post([FromBody]AnalysisInput input)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(AnalysisInput));
+            var stream = new MemoryStream();
+            xmlSerializer.Serialize(stream, input);
+            stream.Position = 0;
+            var inputXml = XDocument.Load(stream);
 
-            // Check if the XML contains the LTL engine           
-            string engineName = inputXml.Descendants("Engine").Elements("Name").First().Value;
+            string engineName = input.Engine;
 
-            var log = new DefaultLogService();
+            //var log = new DefaultLogService();
 
-            var azureLogService = new LogService();
+           // var azureLogService = new LogService();
 
             // SI: Refactor if-clauses into separate methods. 
             if (engineName == "VMCAI")
@@ -36,15 +41,15 @@ namespace bma.client
                     var analyisStartTime = DateTime.Now;
 
                     // Call the Analyzer and get the Output Xml
-                    if (input.EnableLogging)
-                    {
-                        analyzer.LoggingOn(log);
-                    }
-                    else
-                    {
+                   // if (input.EnableLogging)
+                   // {
+                   //     analyzer.LoggingOn(log);
+                   // }
+                   // else
+                   // {
                         analyzer.LoggingOff();
-                        log.LogDebug("Enable Logging from the Run Proof button context menu to see more detailed logging info.");
-                    }
+                    //    log.LogDebug("Enable Logging from the Run Proof button context menu to see more detailed logging info.");
+                   // }
 
                     var outputXml = analyzer.checkStability(inputXml);
 
@@ -53,39 +58,40 @@ namespace bma.client
                     //azureLogService.Debug("Analyze Output XML", outputXml.ToString());
 
                     var time = Math.Round((DateTime.Now - analyisStartTime).TotalSeconds, 1);
-                    log.LogDebug(string.Format("Analyzer took {0} seconds to run.", time));
+                    //log.LogDebug(string.Format("Analyzer took {0} seconds to run.", time));
 
                     // Convert to the Output Data
-                    var outputData = new AnalysisOutputDTO();
+                    var outputData = new AnalysisOutput();
                     outputData.Status = outputXml.Descendants("Status").FirstOrDefault().Value;
-                    if (outputData.Status != StatusTypes.Stabilizing && outputData.Status != StatusTypes.NotStabilizing)
-                    {
-                        var error = outputXml.Descendants("Error").FirstOrDefault();
-                        outputData.Error = error != null ? error.AttributeString("Msg") : "There was an error in the analyzer";
-                    }
+                    //if (outputData.Status != StatusTypes.Stabilizing && outputData.Status != StatusTypes.NotStabilizing)
+                    //{
+                    //    var error = outputXml.Descendants("Error").FirstOrDefault();
+                    //    outputData.Error = error != null ? error.AttributeString("Msg") : "There was an error in the analyzer";
+                    //}
 
-                    outputData.Time = time;
-                    outputData.ErrorMessages = log.ErrorMessages;
-                    outputData.ZippedXml = ZipHelper.Zip(outputXml.ToString());
-                    outputData.ZippedLog = ZipHelper.Zip(string.Join(Environment.NewLine, log.DebugMessages));
+                    //outputData.Time = time;
+                    //outputData.ErrorMessages = log.ErrorMessages;
+                    //outputData.ZippedXml = ZipHelper.Zip(outputXml.ToString());
+                    //outputData.ZippedLog = ZipHelper.Zip(string.Join(Environment.NewLine, log.DebugMessages));
 
                     return outputData;
                 }
                 catch (Exception ex)
                 {
-                    azureLogService.Debug("Analyze Exception", ex.ToString());
+                  //  azureLogService.Debug("Analyze Exception", ex.ToString());
 
                     // Return an Unknown if fails
-                    var outputData = new AnalysisOutputDTO
+                    var outputData = new AnalysisOutput 
                     {
-                        Status = StatusTypes.Unknown,
+                        Status = "unknown"
+                        /* StatusTypes.Unknown,
                         Error = ex.ToString(),
                         ErrorMessages = log.ErrorMessages,
-                        ZippedLog = ZipHelper.Zip(string.Join(Environment.NewLine, log.DebugMessages))
+                        ZippedLog = ZipHelper.Zip(string.Join(Environment.NewLine, log.DebugMessages))*/
                     };
                     return outputData;
                 }
-            }
+            }/*
             else if (engineName == "CAV")
             {
                 // LTL Proof
@@ -233,6 +239,9 @@ namespace bma.client
 
     public class Relationship
     {
+        [XmlAttribute]
+        public int Id { get; set; }
+
         public int FromVariableId { get; set; }
 
         public int ToVariableId { get; set; }
@@ -243,5 +252,18 @@ namespace bma.client
     public class AnalysisOutput
     {
         public string Status { get; set; }
+    }
+
+    public struct StatusTypes
+    {
+         public const string Default = "Default";
+         public const string TryingStabilizing = "TryingStabilizing";
+         public const string Bifurcation = "Bifurcation";
+         public const string Cycle = "Cycle";
+         public const string Stabilizing = "Stabilizing";
+         public const string NotStabilizing = "NotStabilizing";
+         public const string Fixpoint = "Fixpoint";
+         public const string Unknown = "Unknown";
+         public const string Error = "Error";
     }
 }
