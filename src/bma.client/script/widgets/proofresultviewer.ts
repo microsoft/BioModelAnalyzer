e37b80d3cbc23c66a5635f264d2432877b6ecbfc
unknown <innaz@beast.mslab.cs.msu.su>
2014-08-26 12:53:27 +0400

    ProofResultViewer designed initially

--- src/bma.client/script/widgets/proofresultviewer.ts
+++ src/bma.client/script/widgets/proofresultviewer.ts
@@ -14,14 +14,21 @@
             this.element.empty();
 
             var text = "";
-            if (options.issucceeded === undefined)
+            if (options.issucceeded === undefined || options.time === undefined)
                 return;
-            if (options.issucceeded === true)
-                text = "Stabilizes";
-            else text = "Not stabilizes";
-            $('<div>' + text + '</div>').appendTo(that.element);
-            if (options.time !== undefined) {
-                $('<div>' + options.time + '</div>').appendTo(that.element);
+            var table = $('<table></table>').appendTo(that.element);
+            var tr1 = $('<tr></tr>').appendTo(table); 
+            var td1 = $('<td></td>').appendTo(tr1);
+            var td2 = $('<td></td>').appendTo(tr1);
+            if (options.issucceeded) {
+                $('<img src="../../images/succeeded.png">').appendTo(td1);
+                $('<h3 style="color: green; font-weight:bold">Stabilizes</h3>').appendTo(td2);
+                $('<p style="font-size:small">BMA succeeded in checking every possible state of the model in ' + options.time + ' seconds. After stepping through separate interactions, the model eventually reached a single stable state.</p>').appendTo(that.element);
+            }
+            else {
+                $('<img src="../../images/failed.png">').appendTo(td1);
+                $('<h3 style="color: red; font-weight:bold">Failed to Stabilize</h3>').appendTo(td2);
+                $('<p style="font-size:small">After stepping through separate interactions in the model, the analisys failed to determine a final stable state</p>').appendTo(that.element);
             }
         },
 
