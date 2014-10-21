ba282cc6cb608b59512c13e7223312ee3e49db03
Samin Ishtiaq <sishtiaq@microsoft.com>
2014-10-21 16:35:55 +0100

    Something screwed up. Redo TS fe.

---
+++ TypeScriptHTMLApp2A/Controllers/homeController.ts
@@ -0,0 +1,5 @@
+﻿'use strict';
+
+app.controller('homeController', function ($scope) {
+    $scope.message = "Now viewing home!";
+});
---
+++ TypeScriptHTMLApp2A/app.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="Scripts/typings/angularjs/angular.d.ts" />
+
+var app = angular.module('myApp', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'homeController'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'aboutController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }])
+    .controller('mainController', function ($scope) {
+        $scope.message = "Main Content";
+    });;
+
