c93fd74643a4a1343364b84cd0a9628ad412c8d0
Sergey Berezin <sergey@mstlab.org>
2014-10-24 10:21:08 +0400

    Some XML parsing code in tests

---
+++ src/bma.client/test/XmlParserTests.ts
@@ -0,0 +1,22 @@
+﻿describe("XML parser", () => {
+
+    //beforeEach(() => {
+    //    slider = $("<div></div>");
+    //    slider.bmazoomslider();
+    //});
+
+    //afterEach(() => {
+    //    slider.children().detach();
+    //    slider.bmazoomslider("destroy");
+    //})
+
+    it("reads simple model", done => {
+        $.get("data/2var_unstable.xml", null, "xml").then(data => {
+            var $xml = $(data);
+            var $vars = $xml.children("Model").children("Variables").children("Variable");
+            expect($vars.length).toBe(2);
+            done();
+        });
+    });
+
+})   
