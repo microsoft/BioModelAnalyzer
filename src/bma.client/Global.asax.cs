2aa1fc82d2d4449d91b782b1d2135f287ac3b433
Sergey Berezin <sergey@mstlab.org>
2015-01-05 13:20:07 +0300

    Changes from TypedIAnalyzer are merged into master. Unused NuGet packages are removed. WebApi controllers are moved to separate assembly.

--- src/bma.client/Global.asax.cs
+++ src/bma.client/Global.asax.cs
@@ -19,6 +19,8 @@ namespace bma.client
         {
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer = true; 
             
+            // Force controllers assembly to be loaded
+            var assembly = typeof(bma.client.Controllers.AnalyzeController).Assembly;
             GlobalConfiguration.Configuration.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}"                    
