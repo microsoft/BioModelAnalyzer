78cbbf9fd6cf668628c4c803f329f38d82f4a2d7
Nikita A. Skoblov <b-niskob@microsoft.com>
2015-02-12 13:19:43 +0300

    repository cleanup

--- projects/BioCheckWeb/BioCheckWeb-Html/BioCheck.Client/ViewModel/Proof/CounterExampleInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BioCheck.ViewModel.Proof
-{
-    public abstract class CounterExampleInfo
-    {
-        public int Number { get; set; }
-
-        public string Type { get; set; }
-    }
-}
