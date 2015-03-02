87358d580db9755447b952e72199c0f37fbe5b0a
Samin Ishtiaq <samin.ishtiaq@microsoft.com>
2015-03-02 13:45:37 +0000

    Deleted /TypeScriptHTMLApp2A

--- TypeScriptHTMLApp2A/Controllers/aboutController.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-app.controller('aboutController', function ($scope) {
-    $scope.message = "Now viewing about!";
-});
-//# sourceMappingURL=aboutController.js.map
--- TypeScriptHTMLApp2A/Controllers/homeController.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-app.controller('homeController', function ($scope) {
-    $scope.message = "Now viewing home!";
-});
-//# sourceMappingURL=homeController.js.map
