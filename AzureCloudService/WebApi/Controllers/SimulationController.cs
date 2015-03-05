1f7524e90fcd1878ce069fec5548f5b51082e3ea
Samin Ishtiaq <sishtiaq@microsoft.com>
2015-03-05 10:25:34 +0000

    Use Sergey's pure C# projects.

---
+++ AzureCloudService/WebApi/Controllers/SimulationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Lineage;
+//using Microsoft.WindowsAzure;
+//using Microsoft.WindowsAzure.ServiceRuntime;
+//using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Lineage.Controllers
+{
+    public class SimulationController : ApiController
+    {
+        // POST api/Simulation
+        // SI: update payload and return value
+        public int Post([FromBody]int payload)
+        {
+            return 42;
+        }
+    }
+}
