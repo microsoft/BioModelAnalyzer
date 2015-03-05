89709bbf8734f6d316160bdc9a25ea1e58774a74
Samin Ishtiaq <sishtiaq@microsoft.com>
2015-03-05 17:16:30 +0000

    Bounch back payload.

--- AzureCloudService/WebApp/app.ts
+++ AzureCloudService/WebApp/app.ts
@@ -23,25 +23,30 @@ function run_clicked() {
 
     if (func == "simulate") {
         console.log('func=simulate=' + func);
+        var pgm = $('#pgm').val();
         var s_cond = $('input[name=s_condition]:checked').val();
-        console.log('s_cond:' + s_cond);
+        console.log('pgm:' + pgm);
+        console.log('s_cond: ' + s_cond);
+
+        // SI: is pgm:string[]?
+        var i = new SimulationInput(pgm, s_cond);
+        $.ajax({
+            type: "POST",
+            url: "api/Simulation",
+            data: JSON.stringify(i),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (msg) {
+                var o = msg.Output;
+                $("#log").append(o);
+            },
+            error: function (e) {
+                $("#log").append("error: " + e);
+            }
+        })
+
     }
 
-    var i = new SimulationInput(["x", "y"], "func");
-    $.ajax({
-        type: "POST",
-        url: "api/Simulation",
-        data: JSON.stringify(i),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (msg) {
-            var o = msg.Output; 
-            $("#log").append(o);
-        },
-        error: function (e) {
-            $("#log").append("error: " + e);
-        }
-    })
     
 
 }
