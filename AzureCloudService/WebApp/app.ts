20b214e34a8139fbf9674a1e47a3e2b7f5f7eb32
Samin Ishtiaq <sishtiaq@microsoft.com>
2015-03-05 11:59:01 +0000

    Move clock out of the way.

--- AzureCloudService/WebApp/app.ts
+++ AzureCloudService/WebApp/app.ts
@@ -1,34 +1,6 @@
 ﻿/// <reference path="Scripts\typings\jquery\jquery.d.ts"/>
 /// <reference path="Scripts\typings\jqueryui\jqueryui.d.ts"/>
 
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
 
 class SimulationInput {
     public pgm: string[];
@@ -76,4 +48,32 @@ function run_clicked() {
 
 
 
+// SI: old default code. 
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+};
 
+class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
