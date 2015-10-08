9c388498918745f23d31c373c6ad7c0c20b9acae
nikitaskoblov <b-niskob@microsoft.com>
2015-10-08 14:24:03 +0300

    initial sources for widget

--- src/bma.client/script/widgets/ltl/ltlresultsviewer.ts
+++ src/bma.client/script/widgets/ltl/ltlresultsviewer.ts
@@ -3,92 +3,42 @@
 
 (function ($) {
     $.widget("BMA.ltlresultsviewer", {
-        _simulationTables: undefined,
-        _simulationSmallTable: undefined,
-        _simulationBigTable: undefined,
-        _simulationGraph: undefined,
+        _plot: undefined,
+        _variables: undefined,
+        _table: undefined,
 
         options: {
-            //header: [],
-            //numericData: undefined,
-            //colorData: undefined,
-            data: undefined,
-            init: undefined,
-            interval: undefined,
-            variables: undefined,
-            num: 10,
-            buttonMode: "ActiveMode",
-            step: 10,
-            colors: undefined,
+            data: [],
+            init: [],
+            variables: [],
         },
 
         _create: function () {
             var that = this;
+            this.element.empty();
             this.element.addClass("ltlresultsviewer");
 
-            this._simulationTables = $("<div></div>").addClass("ltl-results").addClass("scrollable-results").appendTo(this.element);
-            this._simulationSmallTable = $("<div></div>").addClass("ltl-results-tables").addClass('small-simulation-popout-table').appendTo(this._simulationTables);
-            this._simulationBigTable = $("<div></div>").addClass("ltl-results-tables").addClass('big-simulation-popout-table').appendTo(this._simulationTables);
+            /*
+            <div class="ltlresultsviewer">
+                <div id="coloredTable" class="small-simulation-popout-table"></div>
+                <div id="progressionTable" class="big-simulation-popout-table simulation-progression-table-container"></div>
+                <div class="ltl-simplot-container">
+                    <div id="simulationPlot" class="ltl-results"></div>
+                </div>
+            </div>
+            */
+            var root = this.element;
+            this._variables = $("<div></div>").addClass("small-simulation-popout-table").appendTo(root);
+            this._table = $("<div></div>").addClass("big-simulation-popout-table").addClass("simulation-progression-table-container").appendTo(root);
 
-            this._simulationGraph = $("<div></div>").addClass("ltl-results").appendTo(this.element);
+            var plotContainer = $("<div></div>").addClass("ltl-simplot-container").appendTo(root);
+            this._plot = $("<div></div>").addClass("ltl-results").appendTo(root);
 
-            var stepsdiv = $('<div></div>').addClass('steps-container').appendTo(this._simulationTables);
-
-            //this._simulationSmallTable.coloredtableviewer();
-            this._simulationBigTable.progressiontable({ canEditInitialValue: false });
-            this._simulationGraph.simulationplot();
-
-            var step = this.options.step;
-
-            var stepsul = $('<ul></ul>').addClass('button-list').appendTo(stepsdiv);
-            var li = $('<li></li>').addClass('action-button-small grey').appendTo(stepsul);
-            var li0 = $('<li></li>').appendTo(stepsul);
-            var li1 = $('<li></li>').addClass('steps').appendTo(stepsul);
-            var li2 = $('<li></li>').appendTo(stepsul);
-            var li3 = $('<li></li>').addClass('action-button green').appendTo(stepsul);
-
-            var exportCSV = $('<button></button>')
-                .text('EXPORT CSV')
-                .appendTo(li);
-            exportCSV.bind('click', function () {
-                window.Commands.Execute('ExportCSV', {});
-            })
-            var add10 = $('<button></button>').text('+ ' + step).appendTo(li0);
-            add10.bind("click", function () {
-                that._setOption("num", that.options.num + step);
-            });
-
-            this.num = $('<button></button>').text('STEPS: ' + that.options.num).appendTo(li1);
-            var min10 = $('<button></button>').text('- ' + step).appendTo(li2);
-            min10.bind("click", function () {
-                that._setOption("num", that.options.num - step);
-            })
-            this.RunButton = $('<button></button>').addClass('run-button').text('Run').appendTo(li3);
-
-            this.refresh();
         },
 
         _setOption: function (key, value) {
             var that = this;
             switch (key) {
-                //case "header": {
-                //    this.options.header = value;
-                //    if (this.options.numericData !== undefined && this.options.colorData !== undefined)
-                //        this._simulationTable.coloredtableviewer({ header: value });
-                //    break;
-                //}
-                //case "numericData": {
-                //    this.options.numericData = value;
-                //    if (this.options.header.length != 0 && this.options.colorData !== undefined)
-                //        this._simulationTable.coloredtableviewer({ numericData: value });
-                //    break;
-                //}
-                //case "colorData": {
-                //    this.options.colorData = value;
-                //    if (this.options.numericData !== undefined && this.options.header.length != 0)
-                //        this._simulationTable.coloredtableviewer({ colorData: value });
-                //    break;
-                //}
 
                 case "data": {
                     this.options.data = value;
@@ -96,47 +46,7 @@
                         this._simulationBigTable.progressiontable({ interval: that.options.interval, data: value, canEditInitialValue: false });
                     break;
                 }
-                case "init": {
-                    this.options.init = value;
-                    this._simulationBigTable.progressiontable({ init: value, canEditInitialValue: false });
-                    break;
-                }
-                case "num": {
-                    if (value < 0) value = 0;
-                    this.options.num = value;
-                    this.num.text('STEPS: ' + value);
-                    break;
-                }
-                case "variables": {
-                    this.options.variables = value;
-                    //this._simulationSmallTable.coloredtableviewer({
-                    //    header: ["Graph", "Name", "Range"],
-                    //    type: "graph-max",
-                    //    numericData: that.options.variables
-                    //});
-                    break;
-                }
-                case "interval": {
-                    this.options.interval = value;
-                    this._simulationBigTable.progressiontable({ interval: value, canEditInitialValue: false });
-                    break;
-                }
-                case "buttonMode": {
-                    this.options.buttonMode = value;
-                    this._simulationBigTable.progressiontable({ buttonMode: value, canEditInitialValue: false });
-                    break;
-                }
-                case "step": {
-                    this.options.step = value;
-                    this._simulationBigTable.progressiontable({ step: value, canEditInitialValue: false });
-                    break;
-                }
-
-                case "colors": {
-                    this.options.colors = value;
-                    this._simulationGraph.simulationplot({ colors: value });
-                    break;
-                }
+                
                 default: break;
             }
             this._super(key, value);
@@ -149,20 +59,7 @@
 
         refresh: function () {
             var that = this;
-            //this._simulationSmallTable.coloredtableviewer({
-            //    header: ["Graph", "Name", "Range"],
-            //    type: "graph-max",
-            //    numericData: that.options.variables
-            //});
-            this._simulationBigTable.progressiontable({
-                init: that.options.init,
-                interval: that.options.interval,
-                data: that.options.data,
-                canEditInitialValue: false
-            });
-            //if (this.options.numericData !== undefined && this.options.header.length != 0 && this.options.colorData)
-            //    this._simulationTable.coloredtableviewer({ header: this.options.header, numericData: this.options.numericData, colorData: this.options.colorData });
-            this._simulationGraph.simulationplot({ colors: this.options.colors });
+            
         },
 
     });
