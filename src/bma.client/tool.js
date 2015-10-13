43c220b35383427d2b70e7a46b5ed25a0f600ca2
nikitaskoblov <b-niskob@microsoft.com>
2015-10-13 12:48:36 +0300

    fixed export CSV

--- src/bma.client/tool.js
+++ src/bma.client/tool.js
@@ -11342,7 +11342,7 @@ var BMA;
                     }
                     csv += variable.Name + sep;
                     for (var j = 0; j < ticks.length; j++) {
-                        var tick = ticks[i].Variables;
+                        var tick = ticks[j].Variables;
                         for (var k = 0; k < tick.length; k++) {
                             var ij = tick[k];
                             if (ij.Id === variable.Id) {
