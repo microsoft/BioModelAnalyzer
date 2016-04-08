6b8c93b323f766085f66882d996464622b630a85
Nikita A. Skoblov <b-niskob@microsoft.com>
2016-04-08 16:01:48 +0300

    added DPI correct handling for canvas rendering

--- src/bma.package/script/widgets/ltl/tpviewer.ts
+++ src/bma.package/script/widgets/ltl/tpviewer.ts
@@ -19,13 +19,13 @@
             this.attentionDiv = $("<div></div>").addClass("state-compact").appendTo(root);
             $("<div>+</div>").addClass("state-button-empty").addClass("new").appendTo(this.attentionDiv);
             $("<div>start by defining some temporal properties</div>").addClass("state-placeholder").appendTo(this.attentionDiv);
-            
+
             that.canvasDiv = $("<div></div>").width(root.width()).appendTo(root);
-            that._canvas = $("<canvas></canvas>").attr("width", root.width()).attr("height", root.height()).appendTo(that.canvasDiv);
+            that._canvas = $("<canvas></canvas>").attr("width", root.width()).attr("height", root.height()).width(root.width()).appendTo(that.canvasDiv);
 
             that._stripesImg = new Image();
             that._stripesImg.src = "images/stripe-pattern.png";
-            
+
             that._stripesImg.onload = () => {
                 that.refresh();
             }
@@ -40,8 +40,20 @@
             var padding = { x: 5, y: 10 };
             var maxHeight = keyFrameSize * 4;
             var context = canvas.getContext("2d");
+
+            var PIXEL_RATIO = (function () {
+                var dpr = window.devicePixelRatio || 1;
+                var bsr = (<any>context).webkitBackingStorePixelRatio ||
+                    (<any>context).mozBackingStorePixelRatio ||
+                    (<any>context).msBackingStorePixelRatio ||
+                    (<any>context).oBackingStorePixelRatio ||
+                    (<any>context).backingStorePixelRatio || 1;
+
+                return dpr / bsr;
+            })();
+
             canvas.height = canvas.height;
-            
+
 
             var operations = this.options.operations;
             var currentPos = { x: 0, y: 0 };
@@ -97,10 +109,18 @@
                 sizes.push({ size: opSize, offset: offset, scale: scale });
                 height += opSize.height + this.options.padding.y;
             }
-            canvas.height = height;
+            if (PIXEL_RATIO !== 1) {
+                canvas.height = height * PIXEL_RATIO;
+                canvas.width = that._canvas.width() * PIXEL_RATIO;
+                that._canvas.height(height);
+                context.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
+            } else {
+                canvas.height = height;
+            }
+
             //context.msImageSmoothingEnabled = true;
             context.translate(0.5, 0.5);
-            
+
 
             height = this.options.padding.y;
             for (var i = 0; i < operations.length; i++) {
@@ -148,15 +168,15 @@
                     var context = canvas.getContext("2d");
                     return context.createPattern(this._stripesImg, "repeat");
 
-                    /*var gradient = context.createLinearGradient(-width / 2, 0, width, height);
-                    var n = 20;
-                    for (var i = 0; i < n; i++) {
-                        gradient.addColorStop(i / n, "rgb(217,255,182)");
-                        gradient.addColorStop((2 * i + 1) / (2 * n), "white");
-                    }
-                    return gradient;
-                    */
-                    //return "rgb(217,255,182)";
+                /*var gradient = context.createLinearGradient(-width / 2, 0, width, height);
+                var n = 20;
+                for (var i = 0; i < n; i++) {
+                    gradient.addColorStop(i / n, "rgb(217,255,182)");
+                    gradient.addColorStop((2 * i + 1) / (2 * n), "white");
+                }
+                return gradient;
+                */
+                //return "rgb(217,255,182)";
                 case "fail":
                     return "rgb(254, 172, 158)";
                 default:
