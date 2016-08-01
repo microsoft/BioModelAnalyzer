b21aa3273d11d96feee12e0b7cb4db896752e990
Dmitry Voytsekhovskiy <v-dmvo@microsoft.com>
2016-08-01 12:53:59 +0300

    Web API Analyze and Simulate rin in-process

--- src/BMAWebApi/Controllers/SimulateController.cs
+++ src/BMAWebApi/Controllers/SimulateController.cs
@@ -1,5 +1,6 @@
 ﻿using BioCheckAnalyzerCommon;
 using BioModelAnalyzer;
+using bma.BioCheck;
 using BMAWebApi;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
@@ -17,16 +18,27 @@ using System.Xml.Serialization;
 
 namespace bma.client.Controllers
 {
-    public class SimulateController : JobController
+    public class SimulateController : ApiController
     {
+        private readonly IFailureLogger faultLogger;
 
-        public SimulateController(IFailureLogger logger) : base("Simulate.exe", logger)
+
+        public SimulateController(IFailureLogger logger)
         {
+            if (logger == null) throw new ArgumentNullException("logger");
+            this.faultLogger = logger;
         }
 
-        public Task<HttpResponseMessage> Post()
+        public SimulationOutput Post([FromBody]SimulationInput input)
         {
-            return ExecuteAsync((int)Utilities.GetTimeLimitFromConfig().TotalMilliseconds);
+            var log = new DefaultLogService();
+            var output = Utilities.RunWithTimeLimit(() => Simulation.Simulate(input), Utilities.GetTimeLimitFromConfig());
+            if (output.ErrorMessages != null && output.ErrorMessages.Length > 0)
+            {
+                var contents = new LogContents(output.DebugMessages, output.ErrorMessages);
+                faultLogger.Add(DateTime.Now, typeof(JobController).Assembly.GetName().Version.ToString(), input, contents);
+            }
+            return output;
         }
     }
 }
