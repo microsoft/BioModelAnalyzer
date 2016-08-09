8e2e52f71495f794f5d8f3331ae2e153447cb1b1
Zara Ahmed <t-zaahme@microsoft.com>
2016-08-09 16:50:20 +0000

    added strings for each operator, still needs updating

--- src/dialogs/strings.ts
+++ src/dialogs/strings.ts
@@ -9,3 +9,17 @@ export let TUTORIAL_START_PROMPT = 'Do you like to start the tutorial?'
 export let TUTORIAL_SELECT_CANCELLED = 'Tutorial selection cancelled.'
 export let TUTORIAL_UNKNOWN_SELECT = 'Please input a tutorial number, or anything else to cancel.'
 export let ABOUT_BOT = 'Hello, I am the BMA Bot and I will assist you in creating LTL formulas to query your model. To help you along the way, I will provide you with tutorials and examples.'
+
+//TODO explain each operator below and confirm with BMA users that the definitions are sufficient
+
+export let EXPLAIN_AND = 'this explains the and operator'
+export let EXPLAIN_OR = 'this explains the OR operator'
+export let EXPLAIN_IMPLIES = 'this explains the IMPLIES operator'
+export let EXPLAIN_NOT = 'this explains the NOT operator'
+export let EXPLAIN_NEXT = 'this explains the NEXT operator'
+export let EXPLAIN_ALWAYS = 'this explains the always operator'
+export let EXPLAIN_EVENTUALLY = 'this explains the EVENTUALLY operator'
+export let EXPLAIN_UPTO = 'this explains the UPTO operator'
+export let EXPLAIN_WEAKUNTIL = 'this explains the WEAKUNTIL operator'
+export let EXPLAIN_UNTIL = 'this explains the UNTIL operator'
+export let EXPLAIN_RELEASE = 'this explains the RELEASE operator'
