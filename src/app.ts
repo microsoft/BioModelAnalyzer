d32342788afa7add586a3f3e39bccc98bca1f61b
Maik Riechert <t-mariec@microsoft.com>
2016-08-10 17:49:31 +0100

    fix data storage

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,16 @@ import {setup as setupBot} from './bot'
 let storage = new Storage()
 storage.init()
 
+let botSettings = {
+    // this is false by default but we need to access data between unrelated dialogs
+    persistConversationData: true
+}
+
 let bot: builder.UniversalBot
 if (config.get('USE_CONSOLE')) {
     // Create bot and bind to console
     let connector = new builder.ConsoleConnector().listen()
-    bot = new builder.UniversalBot(connector)
+    bot = new builder.UniversalBot(connector, botSettings)
 } else {
     // Setup Restify Server
     let server = restify.createServer()
@@ -24,7 +29,7 @@ if (config.get('USE_CONSOLE')) {
         appId: config.get<string>('APP_ID'), 
         appPassword: config.get<string>('APP_PASSWORD')
     })
-    bot = new builder.UniversalBot(connector)
+    bot = new builder.UniversalBot(connector, botSettings)
     server.post('/api/messages', connector.listen())
 }
 
