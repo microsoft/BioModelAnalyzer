09342bd227e8d96acb04a13c74b93a81e5739fe7
Maik Riechert <t-mariec@microsoft.com>
2016-08-12 15:22:41 +0100

    make model storage exchangeable

--- src/bot.ts
+++ src/bot.ts
@@ -5,11 +5,12 @@ import {registerMiddleware} from './middleware'
 import {registerLUISDialog} from './dialogs/luis'
 import {registerTutorialDialogs} from './dialogs/tutorials'
 import {registerOtherDialogs} from './dialogs/misc'
+import {ModelStorage} from './ModelStorage'
 
 /** Registers all dialogs and middlewares onto the given bot instance. */
-export function setup (bot: builder.UniversalBot) {
+export function setup (bot: builder.UniversalBot, modelStorage: ModelStorage) {
     registerMiddleware(bot)
-    registerLUISDialog(bot)
+    registerLUISDialog(bot, modelStorage)
     registerTutorialDialogs(bot)
-    registerOtherDialogs(bot)
+    registerOtherDialogs(bot, modelStorage)
 }
