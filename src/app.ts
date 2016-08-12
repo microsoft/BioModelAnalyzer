f34a1b1e7b4e9e7a0db7e6d0c2d2395d151ebefa
Jay Nanavati <t-janana@microsoft.com>
2016-08-12 16:29:22 +0100

    fixed pretty printing bugs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import * as builder from 'botbuilder'
 import * as restify from 'restify'
 import * as config from 'config'
-import {BlobModelStorage} from './ModelStorage'
-import {setup as setupBot} from './bot'
+import { BlobModelStorage } from './ModelStorage'
+import { setup as setupBot } from './bot'
+import NLParser from './NLParser/NLParser'
+
+
+var sentence = "show me a simulation where if x=1 and y=1 then z=1 eventually "
+var model = { "Model": { "Name": "model 1", "Variables": [{ "Name": "x", "Id": 2, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }, { "Name": "y", "Id": 3, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }, { "Name": "z", "Id": 3, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }] } }
+var parserResponse = NLParser.parse(sentence, model)
 
 let server = restify.createServer()
 server.listen(config.get('PORT'), () => {
@@ -24,10 +30,10 @@ if (config.get('USE_CONSOLE')) {
     // Create console bot
     let connector = new builder.ConsoleConnector().listen()
     bot = new builder.UniversalBot(connector, botSettings)
-} else {    
+} else {
     // Create server bot
     let connector = new builder.ChatConnector({
-        appId: config.get<string>('APP_ID'), 
+        appId: config.get<string>('APP_ID'),
         appPassword: config.get<string>('APP_PASSWORD')
     })
     bot = new builder.UniversalBot(connector, botSettings)
---
+++ test/ASTUtils.spec.ts
@@ -0,0 +1,13 @@
+import * as chai from 'chai'
+import NLParser from '../src/NLParser/NLParser'
+var expect = chai.expect;
+
+describe('parse AST to human readable string', function () {
+    it('toHumanReadableString() should return a human readable formula that represents the AST', () => {
+        var sentence = "show me a simulation where if x is 1 and y is equal to 1 then z is 1 in the long run "
+        var model = { "Model": { "Name": "model 1", "Variables": [{ "Name": "x", "Id": 2, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }, { "Name": "y", "Id": 3, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }, { "Name": "z", "Id": 3, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }] } }
+        var parserResponse = NLParser.parse(sentence, model)
+        var expected = "eventually(((x=1 and y=1) implies z=1))"
+        expect(JSON.stringify(parserResponse.humanReadableFormula)).to.equal(JSON.stringify(expected))
+    })
+});
