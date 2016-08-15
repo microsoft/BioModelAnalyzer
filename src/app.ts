485ad1dfb968eaa05c8120bf5eb0aab802193865
Jay Nanavati <t-janana@microsoft.com>
2016-08-15 14:21:04 +0100

    AST pretty printer now handles unary operators

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
 import * as builder from 'botbuilder'
 import * as restify from 'restify'
 import * as config from 'config'
-import { BlobModelStorage } from './ModelStorage'
-import { setup as setupBot } from './bot'
-import NLParser from './NLParser/NLParser'
+import {BlobModelStorage} from './ModelStorage'
+import {setup as setupBot} from './bot'
 
 let port = config.get('PORT')
 console.log('starting on port:', port)
 
 let server = restify.createServer()
-
-
-var model = { "Model": { "Name": "model 1", "Variables": [{ "Name": "y", "Id": 2, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }, { "Name": "x", "Id": 3, "RangeFrom": 0, "RangeTo": 1, "Formula": "" }] } }
-var sentence = "give me some simulation where it is always the case that if x is 1 then y is 5 and in a while x is 5 in the eventual case"
-var parserResponse = NLParser.parse(sentence, model)
-
 server.listen(port, () => {
     console.log('%s listening to %s', server.name, server.url)
 })
@@ -34,10 +27,10 @@ if (config.get('USE_CONSOLE')) {
     // Create console bot
     let connector = new builder.ConsoleConnector().listen()
     bot = new builder.UniversalBot(connector, botSettings)
-} else {
+} else {    
     // Create server bot
     let connector = new builder.ChatConnector({
-        appId: config.get<string>('APP_ID'),
+        appId: config.get<string>('APP_ID'), 
         appPassword: config.get<string>('APP_PASSWORD')
     })
     bot = new builder.UniversalBot(connector, botSettings)
