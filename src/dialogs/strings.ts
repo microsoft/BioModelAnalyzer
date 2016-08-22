6578d282732e1a51ea8408410210c1a5011b4731
Zara Ahmed <t-zaahme@microsoft.com>
2016-08-22 15:14:58 +0100

    unfinished examples for intent OperatorExamples

--- src/dialogs/strings.ts
+++ src/dialogs/strings.ts
@@ -9,7 +9,7 @@ export let TUTORIAL_SELECT_PROMPT = 'Which tutorial would you like to do?'
 export let TUTORIAL_START_PROMPT = 'Would you like to start the tutorial?'
 export let TUTORIAL_SELECT_CANCELLED = 'Tutorial selection cancelled.'
 export let TUTORIAL_UNKNOWN_SELECT = 'Please input a tutorial number, or anything else to cancel.'
-export let ABOUT_BOT = 'Hello, I am the BMA Bot and I will assist you in creating LTL formulas to query your model. To help you along the way, I will provide you with tutorials and examples. Ask me any questions you have about using the BMA and I will try to help you.'
+export let ABOUT_BOT = 'Hello, I am the BMA Bot and I will assist you in creating LTL formulas to query your model. To help you along the way, I will provide you with tutorials and examples. Ask me any questions you have about using LTL within the BMA and I will try to help you.'
 export let ABOUT_SIMULATIONS = 'LTL queries are used to find simulations of a given length that end in a loop (a fix point or cycle)'
 export let SPELLCHECK_ASSUMPTION = (corrected: string) => `I assume you meant: "${corrected}"`
 export let OPEN_BMA_URL = (url: string) => `Open in browser: ${url}`
@@ -29,3 +29,18 @@ export let EXPLAIN_RELEASE = 'This operator requires two operands. In the scenar
 //TODO explain UPTO
 export let EXPLAIN_UPTO = 'This explains the upto operator '
 export let EXPLAIN_WEAKUNTIL = 'Similar to the UNTIL operator, in the example A weakuntil B, A remains true until B holds a true value, however it does not require B to ever hold a true value, and therefore A always remains true' 
+
+//Operator examples
+
+export let EXAMPLE_AND = 'In the example where A and B are equal to a value of 1, A AND B returns true for all traces where both states are 1'
+export let EXAMPLE_OR = 'In the example where A and B are equal to a value of 2, A OR B returns true for all traces where either one of the states is equal to 2'
+export let EXAMPLE_IMPLIES = 'In the example where A is equal to 1 and state B is equal to 2, A implies B returns true for traces where A is 1, and subsequently B is 2'
+export let EXAMPLE_NOT = 'In the example where A is equal to 5, NOT A would return true for all traces that return a value other than 5'
+export let EXAMPLE_NEXT = 'In the example where A is equal to 4, NEXT A returns true for the immediate state before a state where A is 4'
+export let EXAMPLE_ALWAYS = 'In the example where A is equal to 7, ALWAYS A would return true if all the tested traces have a value of 7 for A'
+export let EXAMPLE_EVENTUALLY = 'This works similar to the NEXT operator. For example if we have state A equal to 3, EVENTUALLY A would return true if future states hold a value of 3 for A'
+export let EXAMPLE_UNTIL = 'We have two states; A = 1 and B = 2. A UNTIL B returns true for traces where A holds the value of 1 up until B becomes 2'
+export let EXAMPLE_RELEASE = 'In the example A RELEASE B, where A is 1 and B is 2, B will stay true with a value 2 up until the trace where A is 1. So essentially, at one point both A and B are true, and if A is never 1, then B will hold the value of 2.'
+//TODO example UPTO
+export let EXAMPLE_UPTO = 'Example for upto operator'
+export let EXAMPLE_WEAKUNTIL = 'With states; A = 1 and B = 3, A WEAKUNTIL B implies that A holds a value of 1 up until B is equal to 3, but if B is never 3 then A will remain as 1'
