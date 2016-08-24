f29970de97c32c9116b5bd27b37a3d97d85730a7
Maik Riechert <t-mariec@microsoft.com>
2016-08-24 08:36:02 +0100

    remove parser diagram workaround
    see https://github.com/SAP/chevrotain/issues/268

--- src/NLParser/diagram.ts
+++ src/NLParser/diagram.ts
@@ -1,9 +1,6 @@
 // run this using `npm run diagram` which will compile it and open a browser automatically
 
-// see the diagrams/README subfolder
-//import * as diagrams from 'chevrotain/diagrams/src/main'
-
-import * as diagrams from './diagrams/main'
+import * as diagrams from 'chevrotain/diagrams/src/main'
 import NLParser from './NLParser'
 
 var parserInstanceToDraw = new NLParser([])
