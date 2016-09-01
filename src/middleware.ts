217c783a404cc3c1bb5f98f52c39b472d37bad32
Maik Riechert <t-mariec@microsoft.com>
2016-09-01 18:06:37 +0100

    added formula history core

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,32 @@
 import * as builder from 'botbuilder'
 
 /** 
- * Registers middleware for the given bot.
+ * Registers debug middleware for the given bot with the following functionality:
  * 
- * Currently, this is for debugging purposes such that arbitrary dialogs
- * can be started with the message syntax 'start:/dialogId' while skipping
- * any intent recognizers like LUIS.
+ * 1. Arbitrary dialogs can be started with the message syntax '/dialogId' while skipping
+ *    any intent recognizers like LUIS. Parameters can be passed in JSON syntax,
+ *    e.g. '/dialogId {"foo": "bar"}'.
+ * 
+ * 2. Any dialog can be cancelled with a message starting with 'cancel'.
  */
 export function registerMiddleware (bot: builder.UniversalBot) {
     let debugDialogMiddleware: builder.IMiddlewareMap = {
         botbuilder: (session, next) => {
             let text = session.message.text
-            let debugPrefix = 'start:'
 
-            if (text === 'cancel') {
+            let dialogIdRegEx = /^\/\w+/
+
+            if (text.toLowerCase().indexOf('cancel') === 0) {
                 session.send('OK.')
                 session.cancelDialog(0)
-            } else if (text.match(new RegExp(`^${debugPrefix}.+`))) {
-                let dialogId = text.substr(debugPrefix.length)
-                session.beginDialog(dialogId)
+            } else if (dialogIdRegEx.test(text)) {
+                let firstWhitespaceIdx = text.indexOf(' ')
+                let args
+                if (firstWhitespaceIdx !== -1) {
+                    args = JSON.parse(text.substr(firstWhitespaceIdx + 1))
+                }
+                let dialogId = dialogIdRegEx.exec(text)[0]
+                session.beginDialog(dialogId, args)
             } else {
                 next()
             }
