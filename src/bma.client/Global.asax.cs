3dff70ded0939bd000479f9c12a50e724ee9c078
sberezin <s_berezin@cs.msu.su>
2016-10-18 17:12:11 +0300

    Frontend is converted to Azure Web site.

--- src/bma.client/Global.asax.cs
+++ src/bma.client/Global.asax.cs
@@ -1,21 +1,9 @@
-﻿using BMAWebApi;
-using Microsoft.Practices.Unity;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System.Configuration;
 using System.IO;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Xml.Linq;
 
 namespace bma.client
 {
@@ -38,9 +26,11 @@ namespace bma.client
                 version.Add("build", 0);
             }
 
+            AppSettingsReader asr = new AppSettingsReader();
+
             //Math service URL
             try { 
-                version.Add("computeServiceUrl", RoleEnvironment.GetConfigurationSettingValue("ComputeServiceUrl"));
+                version.Add("computeServiceUrl", (string)asr.GetValue("BackEndUrl", typeof(string)));
             }
             catch
             {
@@ -50,7 +40,7 @@ namespace bma.client
             //OneDrive App ID
             try
             {
-                version.Add("onedriveappid", RoleEnvironment.GetConfigurationSettingValue("OneDriveAppId"));
+                version.Add("onedriveappid", (string)asr.GetValue("LiveAppId", typeof(string)));
             }
             catch
             {
@@ -60,7 +50,7 @@ namespace bma.client
             //OneDrive redirect URL
             try
             {
-                version.Add("onedriveredirecturl", RoleEnvironment.GetConfigurationSettingValue("OneDriveRedirectUrl"));
+                version.Add("onedriveredirecturl", (string)asr.GetValue("RedirectUrl", typeof(string)));
             }
             catch
             {
@@ -77,36 +67,10 @@ namespace bma.client
         {
             GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer = true; 
             
-            // Force controllers assembly to be loaded
-            var assembly = typeof(bma.client.Controllers.AnalyzeController).Assembly;
             GlobalConfiguration.Configuration.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}"                    
             );
-            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
-                name: "TestLRA",
-                routeTemplate: "api/{controller}/{action}/{id}"//,
-                //defaults: new { controller = "TestRLA" }
-            );
-
-            var container = new UnityContainer();
-            IFailureLogger logger;
-            if (RoleEnvironment.IsAvailable)
-                logger = new FailureAzureLogger(
-                            CloudStorageAccount.Parse(
-                                RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString")));
-            else
-                logger = new FailureTraceLogger();
-            container.RegisterInstance<IFailureLogger>(logger);
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityResolver(container);
         }        
-    }
-
-    internal class FailureTraceLogger : IFailureLogger
-    {
-        public void Add(DateTime dateTime, string backEndVersion, object request, ILogContents contents)
-        {
-            Trace.WriteLine(String.Format("Backend {1} fails at {0}", dateTime, backEndVersion));
-        }
     }
 }
