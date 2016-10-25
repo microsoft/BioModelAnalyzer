8efdb189254d5fae534b775f43cff94e29d0d822
Ben Hall <bh418@mrc-cu.cam.ac.uk>
2016-10-25 16:57:31 +0100

    Quick fix for Nir bug

--- src/BioCheckAnalyzer32/GenLemmas.fs
+++ src/BioCheckAnalyzer32/GenLemmas.fs
@@ -40,56 +40,57 @@ let stabilize_lazy (network : QN.node list) =
                     else (n.var, Map.find n.var range) :: bb)
                 []
                 network)
-
-    // The frontier set - variables that have had their bounds updated recently.
-    // Initially, all variables are in the frontier (with no reason). 
-    let mutable frontier = 
-        Set.ofList 
-            (List.fold 
-                (fun bb (n:QN.node) -> 
-                    if (Expr.is_a_const range n.f) then bb
-                    else n.var :: bb)
-                []
-                network)
+    //BH test here to see if we have already got a singleton
+    if Map.isEmpty (Map.filter (fun _ (bottom,top) -> bottom <> top) bounds) then ([(false,bounds)]|>Seq.ofList) else
+        // The frontier set - variables that have had their bounds updated recently.
+        // Initially, all variables are in the frontier (with no reason). 
+        let mutable frontier = 
+            Set.ofList 
+                (List.fold 
+                    (fun bb (n:QN.node) -> 
+                        if (Expr.is_a_const range n.f) then bb
+                        else n.var :: bb)
+                    []
+                    network)
    
-    // initial state for Seq.unfold 
-    let still_working = not (Set.isEmpty frontier) 
-    let initial_state = (still_working,frontier,bounds,outputs)
+        // initial state for Seq.unfold 
+        let still_working = not (Set.isEmpty frontier) 
+        let initial_state = (still_working,frontier,bounds,outputs)
     
-    // The main loop - keep tightening bounds until we can't tighten any further.
-    Seq.unfold 
-       (fun (still_working,frontier,bounds,outputs) ->
-            if still_working then
-                if (not (Set.isEmpty frontier)) then                
-                    // Arbitrarily pick the lowest node in the frontier.
-                    let node = Set.maxElement frontier // SI: Garvit's max, used to be min. 
-                    let frontier = Set.remove node frontier
-                    let expr = Map.find node exprs
-                    let (lower, upper) = Map.find node bounds
+        // The main loop - keep tightening bounds until we can't tighten any further.
+        Seq.unfold 
+           (fun (still_working,frontier,bounds,outputs) ->
+                if still_working then
+                    if (not (Set.isEmpty frontier)) then                
+                        // Arbitrarily pick the lowest node in the frontier.
+                        let node = Set.maxElement frontier // SI: Garvit's max, used to be min. 
+                        let frontier = Set.remove node frontier
+                        let expr = Map.find node exprs
+                        let (lower, upper) = Map.find node bounds
 
-                    let (new_lower, new_upper) = FNewLemmas.tighten node range expr lower upper (Map.find node inputs) bounds
+                        let (new_lower, new_upper) = FNewLemmas.tighten node range expr lower upper (Map.find node inputs) bounds
 
-                    // If we were able to tighten the bound on this variable, then add keep a note of the new bounds,
-                    // and add any dependent variables to the frontier. 
-                    let bounds,frontier = 
-                        if new_upper < upper || new_lower > lower then
-                            let bounds' = Map.add node (new_lower, new_upper) (Map.remove node bounds)
-                            let frontier' = Set.fold (fun fr o -> Set.add o fr) frontier (Map.find node outputs)
-                            Log.log_debug ("Tightened var(" + (string)node + "). Adding it's outputs to frontier")
-                            (bounds',frontier')
-                        else bounds, frontier
-                    Some ((true,bounds),(true,frontier,bounds,outputs))
+                        // If we were able to tighten the bound on this variable, then add keep a note of the new bounds,
+                        // and add any dependent variables to the frontier. 
+                        let bounds,frontier = 
+                            if new_upper < upper || new_lower > lower then
+                                let bounds' = Map.add node (new_lower, new_upper) (Map.remove node bounds)
+                                let frontier' = Set.fold (fun fr o -> Set.add o fr) frontier (Map.find node outputs)
+                                Log.log_debug ("Tightened var(" + (string)node + "). Adding it's outputs to frontier")
+                                (bounds',frontier')
+                            else bounds, frontier
+                        Some ((true,bounds),(true,frontier,bounds,outputs))
 
-                // We've tightened our bounds as much as we can - either every variable has a singleton
-                // bound now (i.e. it's stabilized) or there's at least one variable with a non-constant
-                // bound, in which case we haven't proved anything.
-                else
-                    Some ((false,bounds),(false,frontier,bounds,outputs))
-                //          ^                           indicate to client that this is the last proof step
-                //                         ^ indicate to next Seq.unfold that we're done
-            else 
-                None)
-        initial_state
+                    // We've tightened our bounds as much as we can - either every variable has a singleton
+                    // bound now (i.e. it's stabilized) or there's at least one variable with a non-constant
+                    // bound, in which case we haven't proved anything.
+                    else
+                        Some ((false,bounds),(false,frontier,bounds,outputs))
+                    //          ^                           indicate to client that this is the last proof step
+                    //                         ^ indicate to next Seq.unfold that we're done
+                else 
+                    None)
+            initial_state
 
 /// Attempt to find the unique fixpoint of a QN network.
 /// But do so lazily, returning a sequence of tighter bounds until the final answer
