4a01eb690412628ddd761cb117e8b6edd02aeb40
Dmitry Kats <dmitryk@itis.cs.msu.ru>
2016-12-13 17:06:33 +0300

    Added a script to download and run paket.

---
+++ dl-deps.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest https://github.com/fsprojects/Paket/releases/download/3.31.2/paket.bootstrapper.exe -OutFile .\.paket\paket.bootstrapper.exe
+.\.paket\paket.bootstrapper.exe --run install
